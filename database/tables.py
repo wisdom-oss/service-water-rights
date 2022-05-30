41ed181c7b2f6040524e67732f2d7000a331a82d
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-05-30 12:22:32 +0200

    Added table schemas to the code

---
+++ database/tables.py
@@ -0,0 +1,32 @@
+import geoalchemy2
+import sqlalchemy
+
+import database
+
+geospatial_metadata = sqlalchemy.MetaData(schema="geodata")
+water_right_metadata = sqlalchemy.MetaData(schema="water_rights")
+
+shapes = sqlalchemy.Table(
+    "shapes",
+    geospatial_metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("name", sqlalchemy.Text),
+    sqlalchemy.Column("key", sqlalchemy.Text),
+    sqlalchemy.Column("nuts_key", sqlalchemy.Text),
+)
+
+locations = sqlalchemy.Table(
+    "usage_locations",
+    water_right_metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("water_right", sqlalchemy.Integer, sqlalchemy.ForeignKey("water_rights.no")),
+    sqlalchemy.Column("active", sqlalchemy.Boolean),
+    sqlalchemy.Column("location", geoalchemy2.Geometry("Point", 25832)),
+    sqlalchemy.Column("real", sqlalchemy.Boolean)
+)
+
+
+def init():
+    geospatial_metadata.create_all(bind=database.engine)
+    water_right_metadata.create_all(bind=database.engine)
+
