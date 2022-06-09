5c3716123e20afd413e15a919d91ee415ed73a43
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-06-09 15:58:33 +0200

    Trying to use first row only for determination of last update

--- tools/__init__.py
+++ tools/__init__.py
@@ -32,13 +32,9 @@ async def is_host_available(host: str, port: int, timeout: int = 10) -> bool:
 
 def get_last_schema_update(schema_name: str, engine: sqlalchemy.engine.Engine) -> datetime.datetime:
     query = (
-        f"SELECT timestamp "
-        f"FROM public.audit "
-        f"WHERE schema_name = '{schema_name}' "
-        f"ORDER BY timestamp DESC "
-        f"LIMIT 1"
+        f"SELECT timestamp " f"FROM public.audit " f"WHERE schema_name = '{schema_name}' " f"ORDER BY timestamp DESC "
     )
-    result = engine.execute(query).all()
+    result = engine.execute(query).first()
     if not result:
         return datetime.datetime.now(tz=tzlocal.get_localzone())
     return datetime.datetime.fromtimestamp(result[0], tz=tzlocal.get_localzone())
