82a2715fafde1f5c65ef1d3eb2f80577e68d59d4
Jan Eike Suchard <jan@suchard.cloud>
2023-05-10 14:19:24 +0200

    🚧 adding struct for storing the usage locations

---
+++ src/structs/responses.go
@@ -0,0 +1,16 @@
+package structs
+
+import geojson "github.com/paulmach/go.geojson"
+
+type UsageLocation struct {
+	// ID is the internal ID of the water right
+	ID int `json:"id"`
+	// WaterRight is the water right number used by Cadenza referenced with the location
+	WaterRight int `json:"water_right"`
+	// IsActive reflects the status of the water right location
+	IsActive bool `json:"active"`
+	// IsReal reflects the reality of the water right location
+	IsReal bool `json:"real"`
+	// Location contains the GeoJSON representation of the usage location
+	Location geojson.Geometry `json:"geojson"`
+}
