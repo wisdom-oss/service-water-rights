1607eb4088c9a5321179efa6e1213ed1e283153c
Jan Eike Suchard <jan@suchard.cloud>
2024-01-30 12:59:05 +0100

    updating sql queries to match new dataschema

--- resources/queries.sql
+++ resources/queries.sql
@@ -1,66 +1,48 @@
--- name: create-water-right-schema
-CREATE TABLE IF NOT EXISTS water_rights.water_rights
-(
-    id                    bigint NOT NULL PRIMARY KEY,
-    rights_holder         text                       DEFAULT NULL,
-    valid_from            date                       DEFAULT '-infinity'::date,
-    valid_until           date                       DEFAULT 'infinity'::date,
-    status                text                       DEFAULT NULL,
-    legal_title           text                       DEFAULT NULL,
-    water_authority       text                       DEFAULT NULL,
-    registering_authority text                       DEFAULT NULL,
-    granting_authority    text                       DEFAULT NULL,
-    first_granted         date                       DEFAULT NULL,
-    date_of_change        date                       DEFAULT NULL,
-    file_reference        text                       DEFAULT NULL,
-    external_identifier   text                       DEFAULT NULL,
-    subject               text                       DEFAULT NULL,
-    address               text                       DEFAULT NULL,
-    legal_departments     water_rights.departments[] DEFAULT NULL
-);
+-- name: water-rights
+SELECT *
+FROM water_rights.rights;
 
+/*
+ * Filters for the water rights
+ */
+-- name: filter-water-right-legal-department
+(legal_departments && ANY ($1));
+-- name: filter-water-right-ids
+(id = any($1));
 
 -- name: usage-locations
-SELECT id,
-    water_right,
-    name,
-    no,
-    active,
-    real,
-    ST_AsGeoJSON(ST_TRANSFORM(location, 4326))::jsonb AS location
--- basin_no, county,
--- eu_survey_area, field,
--- groundwater_volume,
--- legal_scope, local_sub_district, maintenance_association, municipal_area, plot, real, rivershed, serial_no,
--- top_map_1_25000, water_body,
--- flood_area, water_protection_area, withdrawal_rate, fluid_discharge, irrigation_area, rain_supplement
-FROM nlwkn_water_rights.e_usage_locations;
+SELECT *, st_asgeojson(st_transform(location, 4326))::jsonb AS location
+FROM water_rights.usage_locations;
 
--- name: water-rights
-SELECT id,
-    no,
-    ext_id,
-    file_ref,
-    legal_title,
-    state,
-    subject,
-    address,
-    annotation,
-    bailee,
-    date_of_change,
-    valid,
-    granting_authority,
-    registering_authority,
-    water_authority
-FROM nlwkn_water_rights.water_rights;
-
-
--- name: filter-locations
-ST_CONTAINS
-(ST_COLLECT(ARRAY ((SELECT geom FROM geodata.shapes WHERE KEY = ANY ($1)))),ST_TRANSFORM(LOCATION, 4326));
-
--- name: filter-reality
-    (REAL = $1 OR REAL IS NULL);
-
--- name: filter-state
-    (active = $1 OR active IS NULL);
+/*
+ * Filters for the usage locations
+ */
+-- name: filter-usage-location-known-geometry
+st_contains(
+    st_collect(
+        ARRAY (
+            SELECT geom FROM geodata.shapes WHERE KEY = ANY ($1)
+        )
+    ),
+    st_transform(location, 4326)
+);
+-- name: filter-usage-location-ids
+(id = any($1));
+-- name: filter-usage-location-water-rights
+(water_right = any($1));
+-- name: filter-usage-location-reality
+(real = $1 OR real IS NULL);
+-- name: filter-usage-location-active-state
+(active = $1 OR active IS NULL);
+-- name: filter-usage-location-custom-wkt-geometry
+st_contains(
+    st_setsrid(
+        st_geomfromewkt($1), 4326
+    ),
+    st_transform(location, 4326)
+);
+-- name: filter-usage-location-custom-geojson-geometry
+st_contains(
+    st_geomfromgeojson($1),
+    st_transform(location, 4326)
+)
