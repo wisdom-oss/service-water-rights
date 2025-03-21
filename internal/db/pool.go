ba3f63adfaefda379053c873d50061e9b0b5baff
Jan Eike Suchard <jan@suchard.cloud>
2025-03-21 13:52:19 +0100

    chore(db): implementing registration of custom types

--- internal/db/pool.go
+++ internal/db/pool.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log/slog"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"microservice/internal"
@@ -65,7 +66,43 @@ func Connect() (err error) {
 	slog.Debug("generated connection string", "connString", connectionString)
 
 	slog.Debug("initializing database pool with connection string", "connString", connectionString)
-	pool, err = pgxpool.New(context.Background(), connectionString)
+	pgConfig, err := pgxpool.ParseConfig("")
+	if err != nil {
+		return fmt.Errorf("%s: %w", ErrPoolConfigurationFailed.Error(), err)
+	}
+
+	pgConfig.AfterConnect = func(ctx context.Context, c *pgx.Conn) error {
+		customTypes := []string{
+			"water_rights.numeric_keyed_value",
+			"water_rights.quantity",
+			"water_rights.rate",
+			"water_rights.dam_target",
+			"water_rights.land_record",
+			"water_rights.legal_department",
+			"water_rights.injection_limit",
+		}
+
+		for _, customType := range customTypes {
+			// create the array version of the type
+			customTypeArray := customType + "[]"
+			// now load the custom type from the database
+			customType, err := c.LoadType(ctx, customType)
+			if err != nil {
+				return err
+			}
+			c.TypeMap().RegisterType(customType)
+
+			// now load the array version of the type
+			customType, err = c.LoadType(ctx, customTypeArray)
+			if err != nil {
+				return err
+			}
+			c.TypeMap().RegisterType(customType)
+		}
+		return nil
+	}
+
+	pool, err = pgxpool.NewWithConfig(context.Background(), pgConfig)
 	if err != nil {
 		return fmt.Errorf("%s: %w", ErrPoolConfigurationFailed.Error(), err)
 	}
