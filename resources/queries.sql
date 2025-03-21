ac22baf63a1d44a5b89d726d8f212c496aa76091
Jan Eike Suchard <jan@suchard.cloud>
2025-03-21 13:53:18 +0100

    chore: removing semicolons to make query generation easier

--- resources/queries.sql
+++ resources/queries.sql
@@ -17,11 +17,11 @@ WHERE
 
 -- name: get-locations
 SELECT *, st_asewkb(st_transform(location, 4326)) AS location_ewkb
-FROM water_rights.usage_locations;
+FROM water_rights.usage_locations
 
 
 -- name: get-water-right-usage-locations
 SELECT *, st_asewkb(st_transform(location, 4326)) AS location_ewkb
 FROM water_rights.usage_locations
 WHERE
-    water_right = $1;
+    water_right = $1
